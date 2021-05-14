b4fea92254798e7c22adf0ed52c3a48cddbdcbdf
Nikita Markovkin <nikmark1212@gmail.com>
2021-05-14 21:50:20 +0300

    two commit

--- src/com/company/homework4/Rectangle2.java
+++ src/com/company/homework4/Rectangle2.java
@@ -3,12 +3,15 @@ package com.company.homework4;
 public class Rectangle2 {
     public static void main(String[] args) {
         Rectangle rectangle0 = new Rectangle(5,5,5);
-        System.out.println(rectangle0.toString());
+        System.out.println(rectangle0);
         //
         Rectangle rectangle1 = new Rectangle(5,5,5,5);
-        System.out.println(rectangle1.toString());
+        System.out.println(rectangle1);
 
-        Rectangle rectangle2 = new Rectangle(rectangle1);
-        System.out.println(rectangle2.toString());
+        Rectangle rectangle2 = new Rectangle(12,26);
+        System.out.println(rectangle2);
+
+        Rectangle rectangle3 = new Rectangle(rectangle1);
+        System.out.println(rectangle3);
     }
 }
