e2c29906e6234f1d3b5574ef02e0845335bafd9b
Nikita Markovkin <nikmark1212@gmail.com>
2021-05-21 20:44:15 +0300

    commit

---
+++ src/Homework5/Helpers.java
@@ -0,0 +1,15 @@
+package Homework5;
+
+public class Helpers {
+    static class DistanceHelper {
+        public void calculateDistance(int x1, int x2, int y1, int y2) {
+        }
+    }
+
+    static class MathHelper {
+        public void sum(int a, int b) {
+
+        }
+    }
+
+}
---
+++ src/Homework5/HelpersTest.java
@@ -0,0 +1,16 @@
+package Homework5;
+
+public class HelpersTest {
+
+    public static void main(String[] args) {
+
+        Helpers.MathHelper helpers = new Helpers.MathHelper();
+
+        helpers.sum(1, 6);
+
+        Helpers.DistanceHelper helpers1 = new Helpers.DistanceHelper();
+
+        helpers1.calculateDistance(5, 5, 5, 5);
+
+    }
+}
